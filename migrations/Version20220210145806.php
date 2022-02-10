d0a77104afe930250a0dc0348b112e3b0cc93d6c
tom <demagny.t@gmail.com>
2022-02-11 00:48:48 +0100

    feat: create new migration
    modify fixture

---
+++ migrations/Version20220210145806.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220210145806 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category_trick (category_id INT NOT NULL, trick_id INT NOT NULL, INDEX IDX_30C3E65712469DE2 (category_id), INDEX IDX_30C3E657B281BE2E (trick_id), PRIMARY KEY(category_id, trick_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, trick_id INT NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_9474526CF675F31B (author_id), INDEX IDX_9474526CB281BE2E (trick_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trick (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_D8F0A91EF675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE category_trick ADD CONSTRAINT FK_30C3E65712469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE category_trick ADD CONSTRAINT FK_30C3E657B281BE2E FOREIGN KEY (trick_id) REFERENCES trick (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CB281BE2E FOREIGN KEY (trick_id) REFERENCES trick (id)');
+        $this->addSql('ALTER TABLE trick ADD CONSTRAINT FK_D8F0A91EF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category_trick DROP FOREIGN KEY FK_30C3E65712469DE2');
+        $this->addSql('ALTER TABLE category_trick DROP FOREIGN KEY FK_30C3E657B281BE2E');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CB281BE2E');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE category_trick');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE trick');
+        $this->addSql('ALTER TABLE messenger_messages CHANGE body body LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE headers headers LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE queue_name queue_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user DROP created_at, CHANGE email email VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,16 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Comment;
 use App\Entity\Trick;
 use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
 class AppFixtures extends Fixture
 {
     const CATEGORIES = [
@@ -19,18 +23,20 @@ class AppFixtures extends Fixture
         'Grabs'
     ];
     const TRICKS_DATA = [
-        [ 'title' => 'Backside air'],
-        [ 'title' => 'Mc Twist'],
-        [ 'title' => 'Cork'],
-        [ 'title' => 'Crippler'],
-        [ 'title' => 'Backside rodeo'],
+        ['title' => 'Backside air'],
+        ['title' => 'Mc Twist'],
+        ['title' => 'Cork'],
+        ['title' => 'Crippler'],
+        ['title' => 'Backside rodeo'],
     ];
 
     public function __construct(
         private UserPasswordHasherInterface $passwordHasher
-    ){}
+    )
+    {
+    }
 
-    public function load(ObjectManager $manager,): void
+    public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
 
@@ -38,39 +44,46 @@ class AppFixtures extends Fixture
             $user = new User;
             $passHashed = $this->passwordHasher->hashPassword($user, 'password');
             $user
-                ->setEmail($faker->email)
+                ->setEmail($faker->email())
                 ->setPassword($passHashed)
                 ->setCreatedAt($faker->dateTimebetween('-7 days'));
 
             $manager->persist($user);
+        }
+        for ($c = 0; $c < count(self::CATEGORIES); ++$c) {
+            $category = new Category();
 
-            for ($c = 0; $c < count(self::CATEGORIES); ++$c) {
-                $category = new Category();
+            $category
+                ->setName(self::CATEGORIES[$c]);
 
-                $category
-                    ->setName(self::CATEGORIES[$c]);
+            $manager->persist($category);
+        }
 
-                $manager->persist($category);
-            }
+        foreach (self::TRICKS_DATA as $t => $titles) {
+           $date = $faker->dateTimeBetween('-2 months');
 
-            for ($t = 0; $t < self::TRICKS_DATA; ++$t) {
-                $titles = self::TRICKS_DATA;
-                $date = $faker->dateTimeBetween('-2 months');
+            $trick = (new Trick())
+                ->setAuthor($user)
+                ->setTitle($t)
+                ->setDescription($faker->paragraph())
+                ->setCreatedAt($date)
+                ->setUpdatedAt($date);
 
-                $trick = (new Trick())
-                    ->setAuthor($user)
-                    ->setTitle($titles['title'])
-                    ->setDescription($faker->paragraph)
-                    ->setCreatedAt($date)
-                    ->setUpdatedAt($date);
+            $manager->persist($trick);
 
-                $manager->persist($trick);
 
-            }
-            for ($co = 0; $co < mt_rand(10, 40); ++$co){
-                $maxDate = max($user->getCreatedAt(), $trick->getCreatedAt());
-}
+        for ($co = 0; $co < mt_rand(5, 20); ++$co) {
 
+            $date = max($user->getCreatedAt(), $trick->getCreatedAt());
+            $comment = new Comment();
+            $comment
+                ->setContent($faker->paragraph())
+                ->setCreatedAt($faker->dateTimeBetween('-' . (new DateTime())->diff($date)->days . 'days'))
+                ->setTrick($trick)
+                ->setAuthor($user);
+
+            $manager->persist($comment);
+        }
         }
 
         $manager->flush();
