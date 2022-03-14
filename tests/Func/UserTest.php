c9444fbd0620d0ec55f363a4f3952aaa77a76232
tom <demagny.t@gmail.com>
2022-03-14 08:45:21 +0100

    modify test to pass the test

--- tests/Func/UserTest.php
+++ tests/Func/UserTest.php
@@ -16,13 +16,13 @@ class UserTest extends AbstractEndpoint
 
         $responseContent = $response->getContent();
         $responseDecoded = json_decode($responseContent);
-dd($response);
+
         self::assertEquals(Response::HTTP_OK, $response->getStatusCode());
         self::assertJson($responseContent);
         self::assertNotEmpty($responseDecoded);
     }
 
-  /*  public function testPostUser(): void
+    public function testPostUser(): void
     {
         $response = $this->getResponseFromRequest(
             Request::METHOD_POST,
@@ -32,12 +32,11 @@ dd($response);
         $responseContent = $response->getContent();
         $responseDecoded = json_decode($responseContent);
 
-        dd($response);
         self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
         self::assertJson($responseContent);
         self::assertNotEmpty($responseDecoded);
     }
-*/
+
     private function getPayload(): string
     {
         $faker = Factory::create();
